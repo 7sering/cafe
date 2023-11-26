e6fa4cf6febee580f82e34dc5c94c0fe4eca67a3
coder <codenow8@gmail.com>
2023-11-26 09:57:12 +0545

    menu desigend

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,59 +1,52 @@
+import { useState } from "react";
+
+import data from "../../data";
+import { useEffect } from "react";
+
 const Menu = () => {
+  const [menus, setMenus] = useState(data);
+
+  useEffect(() => {}, [menus, setMenus]);
   return (
     <>
-      <div className="bg-[#180D0B] h-[350px] text-white">
+      <div className="bg-[#160D0B] py-16 text-white">
         <div className="max-w-6xl mx-auto">
           <h1 className="text-center pt-5 font-bold text-3xl">MENU</h1>
-
+          {/* Buttons */}
           <div className="flex gap-2 justify-center my-3">
-            <button className="bg-orange-900 py-1 px-5 rounded-ss-3xl rounded-br-3xl hover:bg-orange-500 transition-all duration-500 ease-in-out">
+            <button className="bg-orange-900 py-1 px-5 rounded-ss-3xl rounded-br-3xl hover:bg-orange-500 transition-all duration-500 ease-linear">
               Coffee
             </button>
-            <button className="bg-orange-900 py-1 px-5  rounded-ss-3xl rounded-br-3xl hover:bg-orange-500 transition-all duration-500 ease-in-out">
+            <button className="bg-orange-900 py-1 px-5  rounded-ss-3xl rounded-br-3xl hover:bg-orange-500 transition-all duration-500 ease-linear">
               Dessert
             </button>
-            <button className="bg-orange-900 py-1 px-5 rounded-ss-3xl rounded-br-3xl hover:bg-orange-500 transition-all duration-500 ease-in-out">
+            <button className="bg-orange-900 py-1 px-5 rounded-ss-3xl rounded-br-3xl hover:bg-orange-500 transition-all duration-500 ease-linear">
               Brunch
             </button>
           </div>
-          <div className="flex max-w-7xl justify-center">
-            <img
-              src="https://images.pexels.com/photos/1749303/pexels-photo-1749303.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-              alt="coffee"
-              width={200}
-            />
-            <ul className="pl-5">
-              <li>
-                De Late Coffee
-                ...........................................................
-                $5.99
-              </li>
-              <li>
-                Milk Coffee
-                ...........................................................
-                $5.99
-              </li>
-              <li>
-                Cappuccino Coffee
-                ...........................................................
-                $5.99
-              </li>
-              <li>
-                Americano Coffee
-                ...........................................................
-                $5.99
-              </li>
-              <li>
-                Espresso Coffee
-                ...........................................................
-                $5.99
-              </li>
-              <li>
-                Buba Cold Coffee
-                ...........................................................
-                $5.99
-              </li>
-            </ul>
+
+          {/* Card */}
+          <div className="flex flex-wrap  mt-8 ">
+            {menus &&
+              menus.map((menu) => (
+                <div
+                  className="w-xl sm:w-1/2 md:w-1/4 lg:w-1/4 xl:w-1/4 mb-3"
+                  key={menu.id}
+                >
+                  <div className="flex justify-between">
+                    <div className="flex items-center">
+                      <img
+                        src={menu.image}
+                        alt="img"
+                        width="60"
+                        className="rounded-md"
+                      />
+                      <p className="pl-5 text-sm">{menu.title}</p>
+                    </div>
+                    <p className="pt-3 pr-3 text-sm">$ {menu.price}</p>
+                  </div>
+                </div>
+              ))}
           </div>
         </div>
       </div>
