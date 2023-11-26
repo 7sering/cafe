9ed0e273bbed5a62d0f077dbe57099e8d2ccee28
coder <codenow8@gmail.com>
2023-11-26 07:36:30 +0545

    footer and contact us page added

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,11 +1,74 @@
+import Coffee from "../../../public/coffe.png";
+
 const Contact = () => {
   return (
     <>
       <div className="bg-[#180D0B] h-auto">
-        <div className="container mx-auto flex flex-col py-10 justify-center text-white items-center">
-          <h1>BeCafe Pvt.Ltd</h1>
-          <p>Call - 01-1597532</p>
-          <p>Fulbari Boudha - 6</p>
+        <div className="max-w-6xl mx-auto flex flex-col py-10  text-[#fff] ">
+          <div className="flex flex-wrap px-3">
+            <div className="md:w-1/3">
+              <h1 className="text-2xl font-bold text-orange-700">
+                BeCafe Pvt.Ltd
+              </h1>
+              <hr width="50%" className="my-3" />
+              <p className="text-sm pr-8">
+                Welcome to BeCafe Pvt. Ltd, your haven for exceptional coffee
+                experiences. Crafted with passion, our coffee shop
+              </p>
+              <p className="pt-5">Call: 01-7523692</p>
+              <p className="pt-1">Address: Boudha -6, Ktm</p>
+              <p className="pt-1">Email: info@becafe.com.np</p>
+            </div>
+            <div className="md:w-1/3 flex flex-col items-center ">
+              <ul>
+                <h3 className="text-2xl font-bold text-orange-700">Menu</h3>
+                <hr width="100%" className="my-3" />
+                <li className="pb-1">
+                  <a
+                    href="/"
+                    className="hover:text-orange-700 transition-all duration-500 ease-linear"
+                  >
+                    Home
+                  </a>
+                </li>
+                <li className="pb-1">
+                  <a
+                    href="/"
+                    className="hover:text-orange-700 transition-all duration-500 ease-linear"
+                  >
+                    Menu
+                  </a>
+                </li>
+                <li className="pb-1">
+                  <a
+                    href="/"
+                    className="hover:text-orange-700 transition-all duration-500 ease-linear"
+                  >
+                    Gallery
+                  </a>
+                </li>
+                <li className="pb-1">
+                  <a
+                    href="/"
+                    className="hover:text-orange-700 transition-all duration-500 ease-linear"
+                  >
+                    Location
+                  </a>
+                </li>
+                <li className="pb-1">
+                  <a
+                    href="/"
+                    className="hover:text-orange-700 transition-all duration-500 ease-linear"
+                  >
+                    Contact
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="md:w-1/3 flex justify-end ">
+              <img src={Coffee} alt="coffee" width={200} />
+            </div>
+          </div>
         </div>
       </div>
     </>
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,47 @@
+import {
+  FaFacebookSquare,
+  FaInstagram,
+  FaTiktok,
+  FaYoutube,
+  FaTwitter,
+} from "react-icons/fa";
 const Footer = () => {
   return (
     <>
-      <div className="bg-[#241310] text-center text-white">
-        <p className="py-2">
+      <div className="bg-[#241310] text-center text-white py-[3rem]">
+        <div className="flex items-center justify-center pt-3 gap-2">
+          <a
+            href="/facebook"
+            className="hover:text-orange-700 transition-all duration-500 ease-in-out  text-2xl"
+          >
+            <FaFacebookSquare />
+          </a>
+          <a
+            href="/instagram"
+            className="hover:text-orange-700 transition-all duration-500 ease-in-out text-2xl"
+          >
+            <FaInstagram />
+          </a>
+          <a
+            href="/tiktok"
+            className="hover:text-orange-700 transition-all duration-500 ease-in-out text-2xl"
+          >
+            <FaTiktok />
+          </a>
+          <a
+            href="/twitter"
+            className="hover:text-orange-700 transition-all duration-500 ease-in-out text-2xl"
+          >
+            <FaTwitter />
+          </a>
+          <a
+            href="/youtube"
+            className="hover:text-orange-700 transition-all duration-500 ease-in-out text-2xl"
+          >
+            <FaYoutube />
+          </a>
+        </div>
+        <p className="pt-5">
           Copyright © 2019 - {new Date().getFullYear()} &nbsp; BeCafe All Rights
           Reserved.
         </p>
