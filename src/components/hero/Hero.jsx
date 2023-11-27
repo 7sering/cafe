91d13704ec17ef84f341a2495edede60ad44c0c2
coder <codenow8@gmail.com>
2023-11-27 13:33:14 +0545

    hero section mobile view fixed

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,9 +1,9 @@
 const Hero = () => {
   return (
     <>
-      <div className="bg-black/30">
-        <div className="max-w-5xl mx-auto flex flex-col justify-center items-center h-screen">
-          <h1 className="text-8xl text-white uppercase font-bold text-center md:mt-[-90px] ">
+      <div className="bg-black/50">
+        <div className="max-w-5xl mx-auto flex px-8 flex-col justify-center items-center md:h-screen h-[200px]">
+          <h1 className="text-3xl md:text-8xl text-white uppercase font-bold  text-center mt-[-10px] md:mt-[-90px] ">
             Start a day with Fresh Coffee
           </h1>
           <div>
@@ -11,7 +11,7 @@ const Hero = () => {
               Visit for coffee
             </button> */}
 
-            <button className="text-white text-xl bg-orange-800 py-3 px-9 mt-5 rounded-lg hover:bg-orange-600 transition-all duration-500 ease-in-out rounded-ss-[1.6rem] rounded-br-[1.6rem] shadow-md shadow-orange-100">
+            <button className="text-white md:text-xl bg-orange-800 text-1xl py-1 px-5 md:py-3 md:px-9 mt-5 rounded-lg hover:bg-orange-600 transition-all duration-500 ease-in-out rounded-ss-[1.6rem] rounded-br-[1.6rem] shadow-md shadow-orange-100">
               Visit for coffee
             </button>
           </div>
