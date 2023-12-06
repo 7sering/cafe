cfc1ea2be514cad32ea3cf249c0f4f889c2ef735
coder <codenow8@gmail.com>
2023-12-06 09:47:58 +0545

    contact page responsive

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,7 +3,7 @@ import Coffee from "/coffe.png";
 const Contact = () => {
   return (
     <>
-      <div className="bg-[#3e221c] md:py-10">
+      <div className="bg-[#3e221c] md:py-10 p-3">
         <h1 className="text-center text-3xl uppercase font-bold  text-white tracking-widest">
           stay with us
         </h1>
@@ -31,7 +31,8 @@ const Contact = () => {
               <h1 className="text-2xl font-bold text-center md:text-left text-orange-700">
                 BeCafe Pvt.Ltd
               </h1>
-              <hr width="50%" className="my-3" />
+              {/* <hr width="50%" className="my-3" /> */}
+              <hr className="my-3 mx-auto md:mx-0" width="50%" />
               <p className="text-sm pr-8 text-center md:text-left">
                 Welcome to BeCafe Pvt. Ltd, your haven for exceptional coffee
                 experiences. Crafted with passion, at our coffee shop
@@ -44,7 +45,7 @@ const Contact = () => {
                 Email: info@becafe.com.np
               </p>
             </div>
-            <div className="md:w-1/3 md:flex md:flex-col items-center ">
+            <div className="hidden md:w-1/3 md:flex md:flex-col items-center ">
               <ul>
                 <h3 className="text-2xl font-bold text-orange-700 text-center">
                   Menu
