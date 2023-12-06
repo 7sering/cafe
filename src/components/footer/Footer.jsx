23054bf3d8ed7b1fb1f90fda634ba12862aeae36
coder <codenow8@gmail.com>
2023-12-06 09:51:35 +0545

    gap fixed on footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
   return (
     <>
       <div className="bg-[#241310] text-center text-white py-[1rem]">
-        <div className="flex items-center justify-center pt-2 gap-3">
+        <div className="flex items-center justify-center pt-2 gap-5">
           <a
             href="/facebook"
             className="hover:text-orange-700 transition-all duration-500 ease-in-out  text-2xl"
