cd6ed13fa4c00522f3b3d0377a599855491b84f3
coder <codenow8@gmail.com>
2023-12-20 09:52:13 +0545

    basic animation with framer motion

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 const Hero = () => {
   return (
     <>
@@ -10,9 +11,14 @@ const Hero = () => {
         <div className="absolute inset-0 bg-black opacity-50 "></div>
         <div className="absolute inset-0 flex items-center justify-center">
           <div className="max-w-5xl mx-auto text-center">
-            <h1 className="text-2xl md:text-8xl text-white uppercase font-bold mt-[-10px] md:mt-0">
+            <motion.h1
+              initial={{ opacity: 0, y: 100 }}
+              animate={{ opacity: 1, y: 0.5 }}
+              transition={{ delay: 0.5, duration: 1, ease: "easeInOut" }}
+              className="text-2xl md:text-8xl text-white uppercase font-bold mt-[-10px] md:mt-0"
+            >
               Start a day with Fresh Coffee
-            </h1>
+            </motion.h1>
             <div>
               <button className="text-white md:text-xl bg-orange-800 text-1xl py-1 px-5 md:py-3 md:px-9 mt-5 rounded-lg hover:bg-orange-600 transition-all duration-500 ease-in-out rounded-ss-[5rem] rounded-br-[5rem] shadow-md shadow-orange-100">
                 Visit for coffee
